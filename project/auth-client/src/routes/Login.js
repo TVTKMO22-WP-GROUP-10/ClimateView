6736cb5426ffb47b61ddaa2208221844231dbab4
Hannu Karjalainen <hannu.p.karjalainen@gmail.com>
2023-04-17 16:29:29 +0300

    Login raakaversio toimii

--- project/auth-client/src/routes/Login.js
+++ project/auth-client/src/routes/Login.js
@@ -1,12 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import Constants from "../Constants.json"
 
 export default function LoginView() {
+
+  const [loginState, setLoginState] = useState("")
+
+  const handleLoginSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const result = await axios.post(Constants.API_ADDRESS +"/login",
+      null,
+      {
+        params: {
+          uname: event.target.username.value,
+          pw: event.target.password.value
+        }
+      }
+      )
+        .then((response) => {
+          console.log(response.data);
+        })
+
+    } catch (error) {
+      console.error(error);
+    }
+
+
+
+  }
+
   return (
     <div>
-      this is the login page
-      <div>
-        
-      </div>
+      <h1> Login page</h1>
+      <form onSubmit={handleLoginSubmit}>
+        <div>
+          Username <br />
+          <input type="text" name="username"></input>
+        </div>
+        <div>
+          Password <br />
+          <input type="text" name="password"></input>
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
     </div>
   )
 }
