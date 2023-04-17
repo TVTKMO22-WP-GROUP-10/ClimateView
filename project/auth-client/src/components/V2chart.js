c32cf97cd56f3fc5a22cd2049de91fc4d46884e6
s5kike00 <t1kike00@students.oamk.fi>
2023-04-17 17:20:38 +0300

    V2 kesken, merge päivitys

---
+++ project/auth-client/src/components/V2chart.js
@@ -0,0 +1,9 @@
+import { Line } from 'react-chartjs-2';
+import {Chart as ChartJS,
+     LineElement, CategoryScale,
+      LinearScale, PointElement} from 'chart.js';
+
+      ChartJS.register(
+        LineElement, CategoryScale,
+      LinearScale, PointElement
+      )
---
+++ project/src/main/java/com/security/auth/repo/VisualV2Repos/V2YearlyRepo.java
@@ -0,0 +1,24 @@
+package com.security.auth.repo.VisualV2Repos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+//import org.springframework.stereotype.Repository;
+
+import com.security.auth.data.VisualV2.V2Yearly;
+
+// @Repository
+public interface V2YearlyRepo extends JpaRepository<V2Yearly, Integer>{
+    @Query(value = "select * from v2icecore1", nativeQuery = true)
+    List<V2Yearly> getV2Icecore1();
+
+    @Query(value = "select * from v2icecore2", nativeQuery = true)
+    List<V2Yearly> getV2Icecore2();
+
+    @Query(value = "select * from v2icecore3", nativeQuery = true)
+    List<V2Yearly> getV2Icecore3();
+
+    @Query(value = "select * from v2maunaloa_annual", nativeQuery = true)
+    List<V2Yearly> getV2MaunaloaAnnual();
+}
