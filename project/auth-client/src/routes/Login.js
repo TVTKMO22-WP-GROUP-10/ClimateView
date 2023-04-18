d14bcff2cd64028b3be2112c21b0e75c42054ea2
Hannu Karjalainen <hannu.p.karjalainen@gmail.com>
2023-04-18 18:49:17 +0300

    LoginState tehty

--- project/auth-client/src/routes/Login.js
+++ project/auth-client/src/routes/Login.js
@@ -1,39 +1,60 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import Constants from "../Constants.json"
 
 export default function LoginView() {
 
-  const [loginState, setLoginState] = useState("")
+  const navigate = useNavigate();
+  const [loginState, setLoginState] = useState("idle")
 
   const handleLoginSubmit = async (event) => {
     event.preventDefault();
-
+    setLoginState("processing")
     try {
-      const result = await axios.post(Constants.API_ADDRESS +"/login",
-      null,
-      {
-        params: {
-          uname: event.target.username.value,
-          pw: event.target.password.value
+      const result = await axios.post(Constants.API_ADDRESS + "/login",
+        null,
+        {
+          params: {
+            uname: event.target.username.value,
+            pw: event.target.password.value
+          }
         }
-      }
       )
       localStorage.setItem("token", result.data)
-      console.log(result);
-      console.log("XXXX");
-       
+      console.log("Response of post request: " + result);
+      setLoginState("loginSuccess")
+      setTimeout(() => {
+        navigate('/', { replace: true });
+      }, 1500)
+
+
     } catch (error) {
       console.error(error);
+      setLoginState("loginFailure")
     }
-
-
-
   }
+
+  let loginUIControls = null;
+    
+    switch (loginState) {
+      case "idle":
+        loginUIControls = <button type="submit">Login</button>
+        break;
+      case "processing":
+        loginUIControls = <span style={{ color: "blue" }}> Processing...</span>
+        break;
+      case "loginSuccess":
+        loginUIControls = <span style={{ color: "green" }}> Login successful.</span>
+        break;
+      case "loginFailure":
+        loginUIControls = <span style={{ color: "red" }}> Something went wrong.</span>
+        break;
+    }
 
   return (
     <div>
-      <h1> Login page</h1>
+      <h1> Login here</h1>
       <form onSubmit={handleLoginSubmit}>
         <div>
           Username <br />
@@ -44,7 +65,7 @@ export default function LoginView() {
           <input type="text" name="password"></input>
         </div>
         <div>
-          <button type="submit">Login</button>
+          {loginUIControls}
         </div>
       </form>
     </div>
