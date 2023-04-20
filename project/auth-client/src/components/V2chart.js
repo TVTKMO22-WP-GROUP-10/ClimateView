27779082e9603f6b6199bcd22c922c727842b423
s5kike00 <t1kike00@students.oamk.fi>
2023-04-20 10:57:48 +0300

    chart vihdoin toimii ja vaatii populoimista

--- project/auth-client/src/components/V2chart.js
+++ project/auth-client/src/components/V2chart.js
@@ -1,9 +1,97 @@
 import { Line } from 'react-chartjs-2';
-import {Chart as ChartJS,
-     LineElement, CategoryScale,
-      LinearScale, PointElement} from 'chart.js';
+import {
+  Chart as ChartJS,
+  LineElement, CategoryScale,
+  LinearScale, PointElement
+} from 'chart.js';
+import React, { useState } from "react";
+import axios from 'axios';
+import Constants from "../Constants.json"
 
-      ChartJS.register(
-        LineElement, CategoryScale,
-      LinearScale, PointElement
-      )
+ChartJS.register(
+  LineElement, CategoryScale,
+  LinearScale, PointElement
+);
+
+
+const req1 = axios.get(Constants.API_ADDRESS + "/v2icecore1");
+const req2 = axios.get(Constants.API_ADDRESS + "/v2icecore2");
+const req3 = axios.get(Constants.API_ADDRESS + "/v2icecore3");
+const reqY = axios.get(Constants.API_ADDRESS + "/v2maunaloaAnnual");
+const reqM = axios.get(Constants.API_ADDRESS + "/v2maunaloaMonthly");
+
+
+export default function V2chart() {
+
+  const loading = "loading";
+  const error = "error";
+  const [statusState, setStatusState] = useState(loading);
+
+  if (statusState === loading) {
+    axios.all([req1, req2, req3, reqY, reqM]).then(axios.spread((...responses) => {
+      console.log(responses[0].data);
+      //      console.log(responses[1].data);
+      //      console.log(responses[2].data);
+      //      console.log(responses[3].data);
+      //      console.log(responses[4].data);
+
+      const icecore1 = responses[0].data;
+      //const icecore2 = responses[1].data;
+      //const icecore3 = responses[2].data;
+
+      setStatusState({
+        labels: icecore1.map((data) => data.year),
+        datasets: [
+          {
+            label: "icecore1",
+            data: icecore1.map(data => {
+              return {
+                year: data.year,
+                co2ppm: data.co2ppm
+              };
+            })
+            ,
+            parsing: {
+              xAxisKey: "year",
+              yAxisKey: "co2ppm"
+            },
+            pointRadius: 1,
+
+          }
+        ]
+      });
+    })).catch((err) => {
+      console.log(err)
+      setStatusState(error)
+    }); 
+  }
+  const options = {
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Visualization 2",
+      },
+    }
+  }
+  
+  var showStatus;
+    switch (statusState) {
+      case loading:
+        showStatus = <h1>Loading</h1>
+        break;
+      case error:
+        showStatus = <h1>Error</h1>
+        break;
+      default:
+        showStatus = <Line data={statusState} options={options} />
+
+    }
+    return (
+      <div>
+        {showStatus}
+      </div>
+    );
+}
--- project/auth-client/src/routes/V1-V3.js
+++ project/auth-client/src/routes/V1-V3.js
@@ -1,6 +1,6 @@
 import React from "react";
 import V3chart from "../components/V3chart";
-//import V2chart from "../components/V2chart";
+import V2chart from "../components/V2chart";
 import V1chart from "../components/V1chart";
 
 export default function V1V3Graphs() {
@@ -10,7 +10,7 @@ export default function V1V3Graphs() {
                 <V1chart />
             </div>
             <div>
-                <p>V2chart tähän</p>
+                <V2chart />
             </div>
             <div>
                 {V3chart()}
