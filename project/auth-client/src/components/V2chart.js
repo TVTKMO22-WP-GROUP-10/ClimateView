93fd48765ca7b7505375983b58f546c8225328d2
s5kike00 <t1kike00@students.oamk.fi>
2023-04-24 15:36:05 +0300

    merge update

--- project/auth-client/src/components/V2chart.js
+++ project/auth-client/src/components/V2chart.js
@@ -2,17 +2,16 @@ import { Line } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
   LineElement, CategoryScale,
-  LinearScale, PointElement
+  LinearScale, PointElement,
 } from 'chart.js';
 import React, { useState } from "react";
 import axios from 'axios';
-import Constants from "../Constants.json"
+import Constants from "../Constants.json";
 
 ChartJS.register(
   LineElement, CategoryScale,
   LinearScale, PointElement
 );
-
 
 const req1 = axios.get(Constants.API_ADDRESS + "/v2icecore1");
 const req2 = axios.get(Constants.API_ADDRESS + "/v2icecore2");
@@ -20,6 +19,11 @@ const req3 = axios.get(Constants.API_ADDRESS + "/v2icecore3");
 const reqY = axios.get(Constants.API_ADDRESS + "/v2maunaloaAnnual");
 const reqM = axios.get(Constants.API_ADDRESS + "/v2maunaloaMonthly");
 
+const generatedYears = [];
+function generateData(daaaata) {
+  for (let i = 1000; i < 2023; i++)
+    generatedYears["year"] = i;
+}
 
 export default function V2chart() {
 
@@ -38,12 +42,36 @@ export default function V2chart() {
       const icecore1 = responses[0].data;
       const icecore2 = responses[1].data;
       const icecore3 = responses[2].data;
-//      const maunaloaY = responses[3].data;
-//      const maunaloaM = responses[4].data;
+      const maunaloaY = responses[3].data;
+      //      const maunaloaM = responses[4].data;
 
+      generateData();
       setStatusState({
-//        labels: icecore3.map(data => data.year),
+        //labels: icecore3.map(data => data.year),
         datasets: [
+          {
+            // label: "Maunaloa Annual",
+            // data: maunaloaY.map(data => {
+            //   return {
+            //     year: data.year,
+            //     co2ppm: data.co2ppm
+            //   };
+            // })
+            label: "Maunaloa Annual",
+            data: maunaloaY.map(data => ({
+              time: Date.fromObject({
+                year: data.year
+              }).getFullYear(),
+              co2ppm: data.co2ppm
+            }))
+            ,
+            parsing: {
+              xAxisKey: "year",
+              yAxisKey: "co2ppm"
+            },
+            pointRadius: 1,
+            xAxisID: "year"
+          },
           {
             label: "icecore1",
             data: icecore1.map(data => {
@@ -58,7 +86,7 @@ export default function V2chart() {
               yAxisKey: "co2ppm"
             },
             pointRadius: 1,
-
+            xAxisID: "year"
           },
           {
             label: "icecore2",
@@ -74,6 +102,7 @@ export default function V2chart() {
               yAxisKey: "co2ppm"
             },
             pointRadius: 1,
+            xAxisID: "year"
           },
           {
             label: "icecore3",
@@ -89,15 +118,24 @@ export default function V2chart() {
               yAxisKey: "co2ppm"
             },
             pointRadius: 1,
+            xAxisID: "year"
           }
         ]
       });
     })).catch((err) => {
       console.log(err)
       setStatusState(error)
-    }); 
+    });
   }
   const options = {
+    scales: {
+      x: {
+        ticks: {
+          stepSize: 1.0,
+        },
+        // suggestedMin: 1000,
+        // suggestedMax: 2023
+      },
     plugins: {
       legend: {
         position: "top",
@@ -105,25 +143,26 @@ export default function V2chart() {
       title: {
         display: true,
         text: "Visualization 2",
-      },
+      }
+    }
     }
   }
 
   var showStatus;
-    switch (statusState) {
-      case loading:
-        showStatus = <h1>Loading</h1>
-        break;
-      case error:
-        showStatus = <h1>Error</h1>
-        break;
-      default:
-        showStatus = <Line data={statusState} options={options} />
+  switch (statusState) {
+    case loading:
+      showStatus = <h1>Loading</h1>
+      break;
+    case error:
+      showStatus = <h1>Error</h1>
+      break;
+    default:
+      showStatus = <Line data={statusState} options={options} />
 
-    }
-    return (
-      <div>
-        {showStatus}
-      </div>
-    );
+  }
+  return (
+    <div>
+      {showStatus}
+    </div>
+  );
 }
